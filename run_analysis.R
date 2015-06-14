753c519e13e14a946ce93a0ebde969b6120c8e43
Thomas Mawora <tmawora@users.noreply.github.com>
2015-06-14 02:32:17 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,17 @@
-A script to accomplish the Course project
+#################################################################################################
+#####  Thomas Mawora
+#####  Coursera
+#####  Getting and cleaning data
+#####  Project script
+#################################################################################################
+
+
+#################################################################################################
+## First I shall set up the working directory in my computer
+setwd("C:/Users/Mwakudisa/getting-and-cleaning-data")
+
+## Then I shall download the file and save it in my working directory
+
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url,"datazet.zip")
+unzip(zipfile="datazet.zip",list=TRUE)
